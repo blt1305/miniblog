6797f939317f081f11c463cc09effceb6eb1cbe5
blt1305 <blt1305@gmail.com>
2023-05-18 12:48:49 +0300

    delete app 'blog'

--- miniblog/blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.base import View
-from .models import Post
-
-class PostView(View):
-    '''вывод записей'''
-    def get(self, request):
-        posts = Post.objects.all()
-        return render(request, 'blog/blog.html')
--- miniblog/miniblog/urls.py
+++ miniblog/miniblog/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from miniblog import settings
+from blog.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', index)
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
