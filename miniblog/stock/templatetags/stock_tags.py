83111e7fc52466af3e29fd9eca6f6f869427bfd8
blt1305 <blt1305@gmail.com>
2023-07-26 22:07:31 +0300

    Добавлен пользовательский тег шаблона для контекстного меню (переменная menu)

--- miniblog/stock/templatetags/stock_tags.py
+++ miniblog/stock/templatetags/stock_tags.py
@@ -20,3 +20,27 @@ def show_categories(sort = None, cat_selected = 0):
     else:
         cats = Category.objects.order_by(sort)
     return {'cats': cats, 'cat_selected': cat_selected}
+
+@register.simple_tag()
+def get_menu():
+    '''возвращает контекстное меню'''
+    menu = [
+        {'title': 'Главная', 'url_name': 'home'},
+        {'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}
+    ]
+    return menu
+
+@register.inclusion_tag('stock/list_menu.html')
+def show_menu():
+    '''создание html-фрагмента с контекстным меню'''
+    menu = [
+        {'title': 'Главная', 'url_name': 'home'},
+        {'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}
+    ]
+    return {'menu': menu}
--- miniblog/stock/views.py
+++ miniblog/stock/views.py
@@ -5,37 +5,30 @@ from django.views.generic import DetailView, UpdateView, DeleteView, View
 from .models import *
 from .forms import CommentForm
 
-menu = [
-    {'title':'Главная', 'url_name': 'home'},
-    {'title':'О сайте', 'url_name': 'about'},
-    {'title':'Добавить статью', 'url_name':'add_page'},
-    {'title':'Обратная связь', 'url_name':'contact'},
-    {'title':'Войти','url_name':'login'}
-]
+
 
 def index(request):
     posts = Artifact.objects.all()
     context = {
         'posts': posts,
         'title': 'Главная страница',
-        'menu': menu,
         'cat_selected': 0,}
     return render (request, 'stock/index.html', context=context)
 
 
 def about(request):
-    return render (request, 'stock/about.html', {'menu': menu, 'title': 'О сайте'})
+    return render (request, 'stock/about.html', { 'title': 'О сайте'})
 
 
 def add_page(request):
-    return render (request, 'stock/add_page.html', {'menu': menu, 'title': 'Добавить статью'})
+    return render (request, 'stock/add_page.html', { 'title': 'Добавить статью'})
 
 
 def contact(request):
-    return render (request, 'stock/contact.html', {'menu': menu, 'title': 'Обратная связь'})
+    return render (request, 'stock/contact.html', { 'title': 'Обратная связь'})
 
 def login(request):
-    return render(request, 'stock/login.html', {'menu': menu, 'title': 'Войти'})
+    return render(request, 'stock/login.html', { 'title': 'Войти'})
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
@@ -46,7 +39,6 @@ def show_post(request, post_id):
     context = {
         'post': art,
         'title': art,
-        'menu': menu
     }
     return render(request, 'stock/one_art.html', context=context)
 
@@ -58,7 +50,6 @@ def show_category(request, cat_id):
     context = {
         'posts': posts,
         'title': 'Отображение по категориям',
-        'menu': menu,
         'cat_selected': cat_id, }
     return render (request, 'stock/index.html', context=context)
 
